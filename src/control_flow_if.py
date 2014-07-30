4aab0e361771f1aeec96d72233a4b5babe867093
Andres Rodriguez <anrocox@gmail.com>
2014-07-30 08:29:48 -0500

    How to use the control structure if of Python?

---
+++ src/control_flow_if.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Created on Jul 22, 2014
+
+@author: anroco
+
+How to use the control structure if of Python?
+
+¿Cómo usar la estructura de control if en Python?
+'''
+
+# must define the condition to evaluate and should always end with a colon
+# if expression:
+#    statements
+
+# create a integer
+n = 354
+
+# check if the value of n is greater than 200
+if n > 200:
+    print('the value of n is greater than 200')
+
+# check if the value of n is equal to 200
+if n == 200:
+    print('the value of n is equal to 200')
+print('the value of n is different from 200')
